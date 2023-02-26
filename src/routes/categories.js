dc80c16943684887c6ad364f8372d41ed7a9f12a
AndrePort <aporto-@hotmail.com>
2023-02-26 11:40:06 -0300

    feat: finances instances and reusable queries

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -1,23 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const db = require("../db");
-
-const findOne = (id) => {
-  return (query = {
-    name: "fetch-category",
-    text: "SELECT * FROM categories WHERE id = $1",
-    values: [Number(id)],
-  });
-};
+const categoriesQueries = require("../queries/categories");
 
 router.get("/", (req, res) => {
   try {
-    db.query("SELECT * FROM categories ORDER BY name ASC", (error, response) => {
-      if (error) {
-        return res.status(500).json(error);
+    db.query(
+      "SELECT * FROM categories ORDER BY name ASC",
+      (error, response) => {
+        if (error) {
+          return res.status(500).json(error);
+        }
+        return res.status(200).json(response.rows);
       }
-      return res.status(200).json(response.rows);
-    });
+    );
   } catch (error) {
     return res.status(500).json(error);
   }
@@ -54,7 +50,7 @@ router.delete("/:id", async (req, res) => {
     if (!id) {
       return res.status(400).json({ error: "Param id is mandatory." });
     }
-    const query = findOne(id);
+    const query = categoriesQueries.findById(id);
     const category = await db.query(query);
     if (!category.rows[0]) {
       return res.status(404).json({ error: "Category not found" });
@@ -86,7 +82,7 @@ router.put("/:id", async (req, res) => {
         .json({ error: "Name should have more than 3 characters" });
     }
 
-    const query = findOne(id);
+    const query = categoriesQueries.findById(id);
     const category = await db.query(query);
     if (!category.rows[0]) {
       return res.status(404).json({ error: "Category not found" });
