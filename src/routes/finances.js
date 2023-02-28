d062309fb1d7fdb5c100eb38ea90c9e02de58108
AndrePort <aporto-@hotmail.com>
2023-02-27 23:16:10 -0300

    feat: delete item implemented

--- src/routes/finances.js
+++ src/routes/finances.js
@@ -10,7 +10,7 @@ router.post("/", async (req, res) => {
     const { category_id, title, date, value } = req.body;
 
     if (email.length < 5 || !email.includes("@")) {
-      return res.status(400).json({ error: "E-mail is invalid" });
+      return res.status(400).json({ error: "E-mail is invalid." });
     }
 
     if (!category_id) {
@@ -44,7 +44,7 @@ router.post("/", async (req, res) => {
     }
 
     const text =
-      "INSERT INTO finances(user_id,category_id,date,title,value) VALUES($1.$2,$3,$4,$5) RETURNING *";
+      "INSERT INTO finances(user_id,category_id,date,title,value) VALUES($1,$2,$3,$4,$5) RETURNING *";
     const values = [userQuery.rows[0].id, category_id, date, title, value];
 
     const crateResponse = await db.query(text, values);
@@ -54,8 +54,51 @@ router.post("/", async (req, res) => {
 
     return res.status(200).json(crateResponse.rows[0]);
   } catch (error) {
-    return res.status(500).json(error);
+    return res.status(500).json({ error });
   }
 });
 
+router.delete("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { email } = req.headers;
+
+    if (email.length < 5 || !email.includes("@")) {
+      return res.status(400).json({ error: "E-mail is invalid." });
+    }
+
+    if (!id) {
+      return req.status(400).json({ error: "Id is mandatory" });
+    }
+
+    const userQuery = await db.query(usersQueries.findByEmail(email));
+    if (!userQuery.rows[0]) {
+      return res.status(404).json({ error: "User does not exists." });
+    }
+
+    const findFinanceText = "SELECT * FROM finances WHERE id=$1";
+    const findFinanceValue = [Number(id)];
+    const financeItemQuery = await db.query(findFinanceText, findFinanceValue);
+
+    if (!financeItemQuery.rows[0]) {
+      return res.status(400).json({ error: "Finances row not found" });
+    }
+    if (financeItemQuery.rows[0].user_id !== userQuery.rows[0].id) {
+      return res
+        .status(401)
+        .json({ error: "Finance row does not belong to user" });
+    }
+    const text = "DELETE FROM finances WHERE id=$1 RETURNING *";
+    const values = [Number(id)];
+    const deleteResponse = await db.query(text, values);
+
+    if (!deleteResponse.rows[0]) {
+      return res.status(400).json({ error: "Finances row not deleted" });
+    }
+
+    return res.status(200).json(deleteResponse.rows[0]);
+  } catch (error) {
+    return res.status(500).json({ error });
+  }
+});
 module.exports = router;
