5103b31504559a8acf61d8612898810db694ba43
AndrePort <aporto-@hotmail.com>
2023-03-05 11:40:24 -0300

    feat: configs for deploy

--- src/db.js
+++ src/db.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const { Pool } = require("pg");
 
-const { DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT } = process.env
+const { DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT } = process.env;
 
 const db = new Pool({
   user: DB_USER,
